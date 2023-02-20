bd8be0cebef3d8b1ad75d4de86cd754904af2e72
Mateo Borrazas <mborrazas@roku.com>
2023-02-20 15:24:48 -0300

    resolve issues login 2

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,20 +1,40 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {useAddress, useWeb3} from "../../blockchain/BlockchainContext";
 import {useUser} from "../../contexts/AppContext";
+import { redirect } from 'next/navigation';
+import {useRouter} from "next/router";
+
 
 export default function home() {
+    const router = useRouter();
     const address = useAddress()
     const web3 = useWeb3()
     const user = useUser()
     const [error, setError] = useState(null)
 
+    const shouldRedirect = !user
+
+
+    useEffect(() => {
+        if (shouldRedirect) {
+            router.push('/login');
+        }
+    }, [shouldRedirect, router]);
+
     return (
-        <>
-        <h1>{"Hola " + user.name}</h1>
-        <h2>{"Eres admin? "} {user.isAdmin ? "si" : "no"}</h2>
-        <h2>{"Tu address" + user.address }</h2>
-        </>
-    )
+        <div>
+            {shouldRedirect ? (
+                <p>Redirecting to login page...</p>
+            ) : (
+                <>
+                <h1>{"Hola " + user.name}</h1>
+                <h2>{"Eres admin? "} {user.isAdmin ? "si" : "no"}</h2>
+                <h2>{"Tu address" + user.address}</h2>
+                </>
+            )}
+        </div>
+    );
+
 }
 
 
