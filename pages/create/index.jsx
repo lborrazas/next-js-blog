ccda54b6ffb0b787072f959dd482006b197cb42f
IgnacioCogliatti <jcogliatti@correo.um.edu.uy>
2023-02-24 11:39:43 -0300

    aa

--- pages/create/index.jsx
+++ pages/create/index.jsx
@@ -75,7 +75,7 @@ export default function home({ users }) {
     );
     let a = await vmContract.methods.createCollectible(latitude.current.value, longitude.current.value).send({ from: address })
     const parcela = {
-      address:address,
+      address: address,
       latitud: Number(latitude.current.value),
       longitud: Number(longitude.current.value),
       m2: Number(area.current.value),
@@ -87,17 +87,18 @@ export default function home({ users }) {
 
     const result = await axios.post("/api/parcelacreate", parcela);
     console.log(result)
-    if (result.data == -1){
+    if (result.data == -1) {
       alert('parcela already exist')
-    }else{
-    router.push("/home")
+    } else {
+      router.push("/home")
+    }
   };
 
   return (
-    <Box sx={{ flexGrow: 1 }}> 
+    <Box sx={{ flexGrow: 1 }}>
       {
-      shouldRedirect ? (<RedirectPage /> ) : 
-      <><Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+        shouldRedirect ? (<RedirectPage />) :
+          <><Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
             <Typography component="h1" variant="h4">
               Crear parcela
             </Typography>
@@ -105,97 +106,97 @@ export default function home({ users }) {
               Refresh
             </Button>
           </Stack>
-          <Item  sx={{height: "50vH"}} >
-          <Grid container component="form" onSubmit={handleSave} spacing={1}>
-            <Grid item xs={12}>
-                <InputLabel id="userOwner-label">Dueño</InputLabel>
-                <Select
-                  margin="normal"
-                  value={"null"}
-                  fullWidth
-                  id="userOwner"
-                  label="Dueño"
-                  name="userOwner"
-                  autoComplete="userOwner"
-                >
-                  <MenuItem selected defaultChecked value={"null"}>
-                    Sin asignar
-                  </MenuItem>
-                  {users.map((user) => (
-                    <MenuItem value={user.address}>{user.name}</MenuItem>
-                  ))}
-                </Select>
+            <Item sx={{ height: "50vH" }} >
+              <Grid container component="form" onSubmit={handleSave} spacing={1}>
+                <Grid item xs={12}>
+                  <InputLabel id="userOwner-label">Dueño</InputLabel>
+                  <Select
+                    margin="normal"
+                    value={"null"}
+                    fullWidth
+                    id="userOwner"
+                    label="Dueño"
+                    name="userOwner"
+                    autoComplete="userOwner"
+                  >
+                    <MenuItem selected defaultChecked value={"null"}>
+                      Sin asignar
+                    </MenuItem>
+                    {users.map((user) => (
+                      <MenuItem value={user.address}>{user.name}</MenuItem>
+                    ))}
+                  </Select>
+                </Grid>
+                <Grid item xs={6}>
+                  <TextField
+                    inputRef={latitude}
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="latitude"
+                    label="Latitud"
+                    name="latitude" />
+                </Grid>
+                <Grid item xs={6}>
+                  <TextField
+                    inputRef={longitude}
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="longitude"
+                    label="Longitud"
+                    name="longitude" />
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography component="h3" variant="h4">
+                    Porcentaje de área usada
+                  </Typography>
+                  <Slider
+                    id="areaPercent"
+                    defaultValue={50}
+                    aria-label="Default"
+                    valueLabelDisplay="auto"
+                    ref={sliderRef} />
+                </Grid>
+                <Grid item xs={3}>
+                  <TextField
+                    inputRef={area}
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="area"
+                    label="Área"
+                    name="area" />
+                </Grid>
+                <Grid item xs={3}>
+                  <TextField
+                    inputRef={averageHeight}
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="averageHeight"
+                    label="Altura promedio"
+                    name="averageHeight"
+                    autoComplete="averageHeight" />
+                </Grid>
+                <Grid item xs={3}>
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{
+                      mt: 3,
+                      mb: 2,
+                    }}
+                  >
+                    Guardar
+                  </Button>
+                  {error && <Typography variant="body2">{error}</Typography>}
+                </Grid>
               </Grid>
-              <Grid item xs={6}>
-                <TextField
-                  inputRef={latitude}
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="latitude"
-                  label="Latitud"
-                  name="latitude" />
-              </Grid>
-              <Grid item xs={6}>
-                <TextField
-                  inputRef={longitude}
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="longitude"
-                  label="Longitud"
-                  name="longitude" />
-              </Grid>
-              <Grid item xs={6}>
-                <Typography component="h3" variant="h4">
-                  Porcentaje de área usada
-                </Typography>
-                <Slider
-                  id="areaPercent"
-                  defaultValue={50}
-                  aria-label="Default"
-                  valueLabelDisplay="auto"
-                  ref={sliderRef} />
-              </Grid>
-              <Grid item xs={3}>
-                <TextField
-                  inputRef={area}
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="area"
-                  label="Área"
-                  name="area" />
-              </Grid>
-              <Grid item xs={3}>
-                <TextField
-                  inputRef={averageHeight}
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="averageHeight"
-                  label="Altura promedio"
-                  name="averageHeight"
-                  autoComplete="averageHeight" />
-              </Grid>
-              <Grid item xs={3}>
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  sx={{
-                    mt: 3,
-                    mb: 2,
-                  }}
-                >
-                  Guardar
-                </Button>
-                {error && <Typography variant="body2">{error}</Typography>}
-              </Grid>
-            </Grid>
             </Item>
-            </>
-    }
+          </>
+      }
     </Box >
   );
 }
