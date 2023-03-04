c641bcc8c829c855d827517be17f3663733aefb3
IgnacioCogliatti <jcogliatti@correo.um.edu.uy>
2023-03-04 15:20:01 -0300

    update2

--- components/navbar/navbarLists.js
+++ components/navbar/navbarLists.js
@@ -5,13 +5,15 @@ import HomeIcon from "@mui/icons-material/Home";
 import GrassIcon from "@mui/icons-material/Grass";
 import SyncAltIcon from "@mui/icons-material/SyncAlt";
 import AppsIcon from "@mui/icons-material/Apps";
+import RecentActorsIcon from '@mui/icons-material/RecentActors';
+
 
 export const navItems = [
   { Name: "Inicio", Icon: HomeIcon, Url: "/home" },
   { Name: "Clientes", Icon: GroupsIcon, Url: "" },
   { Name: "Parcelas", Icon: TokenIcon, Url: "/plot" },
   { Name: "Crear Parcela", Icon: GrassIcon, Url: "/create" },
-  { Name: "Lista de Usuarios", Icon: GrassIcon, Url: "/users" },
+  { Name: "Lista de Usuarios", Icon: RecentActorsIcon, Url: "/users" },
   { Name: "Lista de parcelas", Icon: AppsIcon, Url: "/parcelas" },
   { Name: "Metricas", Icon: AutoGraphIcon, Url: "" },
   { Name: "Transfer", Icon: SyncAltIcon, Url: "/transfer" },
