f72e5bf252265a6c842a1b08f85f8d3dbaa8a666
jabuarab <alasjan1@gmail.com>
2023-03-06 03:17:52 -0300

    createModal

--- components/plot/PlotForm.jsx
+++ components/plot/PlotForm.jsx
@@ -10,6 +10,12 @@ import { useState } from "react";
 import axios from "axios";
 import { useVmContract } from "../../blockchain/BlockchainContext";
 import { useRouter } from "next/router";
+import * as React from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 export const PlotForm = ({ selectedPlot, users }) => {
   const router = useRouter();
@@ -17,15 +23,28 @@ export const PlotForm = ({ selectedPlot, users }) => {
 
   const [owner, setOwner] = useState("");
 
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const handleChangeOwner = (e) => {
     setOwner(e.target.value);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const data = new FormData(e.currentTarget);
+    console.log("ENTRO");
+    console.log(e.currentTarget);
+    
+    const data = new FormData(document.getElementById('submitgrid'));
     const plot = {
-      address: owner,
+      address: owner.address,
       latitud: data.get("latitude"),
       longitud: data.get("longitude"),
       m2: data.get("area"),
@@ -34,20 +53,21 @@ export const PlotForm = ({ selectedPlot, users }) => {
       m3: data.get("average-height"),
     };
     const result = await axios.post("/api/parcelacreate", plot);
+    console.log(result)
     // TODO: como que -2 ??????????????
-    if (result.data === -2) {
+    if (result.data === -1) {
       alert("parcela already exist");
     } else {
       // TODO: esto se deberia usar en algun lado?
-      const a = await vmContract.methods
-        .createCollectible(plot.longitud, plot.latitud)
-        .send({ from: plot.address });
-      await router.push("/home");
+      //const a = await vmContract.methods
+       // .createCollectible(plot.longitud, plot.latitud)
+      //  .send({ from: plot.address });
+      //await router.push("/home");
     }
   };
 
   return (
-    <Grid container component="form" onSubmit={handleSubmit} spacing={1}>
+    <Grid container component="form" onSubmit={handleSubmit} spacing={1} id='submitgrid'>
       <Grid item xs={12}>
         <InputLabel id="owner-select">Dueño</InputLabel>
         <Select
@@ -119,18 +139,59 @@ export const PlotForm = ({ selectedPlot, users }) => {
         />
       </Grid>
       <Grid item xs={12} md={3}>
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          sx={{
-            mt: 3,
-            mb: 2,
-          }}
-        >
-          Guardar
-        </Button>
+        {!selectedPlot ? (
+          <Button
+            onClick={handleClickOpen}
+            fullWidth
+            variant="contained"
+            sx={{
+              mt: 3,
+              mb: 2,
+            }}
+          >
+            Guardar
+          </Button>
+        ) : (
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{
+              mt: 3,
+              mb: 2,
+            }}
+          >
+            Guardar
+          </Button>
+        )}
       </Grid>
+      {!selectedPlot ? (
+        <Grid>
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">
+              {"Create Parcela"}
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                Continuar con la creación de la parcela?
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Disagree</Button>
+              <Button onClick={handleSubmit} variant="contained" autoFocus>
+                Agree
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </Grid>
+      ) : (
+        <div></div>
+      )}
     </Grid>
   );
 };
